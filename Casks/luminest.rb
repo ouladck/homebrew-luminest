a8c6e82233bf98c4515519e13a416e52ac1e1e0e
Karim Oulad Chalha <ouladchalha.k@gmail.com>
2026-03-03 21:57:47 +0100

    Update luminest.rb for new app structure

--- Casks/luminest.rb
+++ Casks/luminest.rb
@@ -1,13 +1,13 @@
 cask "luminest" do
   version "1.0.0-RC"
-  sha256 "cb5f6433510c06c3b1830855316dc4c07d6bbbb82a29ae7fa525fa5d722b3e97"
+  sha256 "0bbb2180b46b55c091eca768f8fc4e790885b21a030210d77a3b40cf00153170"
 
   url "https://github.com/ouladck/LumiNest/releases/download/v#{version}/LumiNest-#{version}.zip"
   name "LumiNest"
   desc "Fast local photo and video browser for macOS"
   homepage "https://github.com/ouladck/LumiNest"
 
-  binary "Release/LumiNest", target: "luminest"
+  app "LumiNest.app"
 
   zap trash: [
     "~/Library/Application Support/LumiNest",
