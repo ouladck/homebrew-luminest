c4a8eeb52238686d54e115ee4075e018d318eff0
Karim Oulad Chalha <ouladchalha.k@gmail.com>
2026-03-09 01:19:27 +0100

    Fix URL format in luminest.rb cask

--- Casks/luminest.rb
+++ Casks/luminest.rb
@@ -2,7 +2,7 @@ cask "luminest" do
   version "1.0.1-RC"
   sha256 "ad9acdbf5664102378bdbed30811867bab5dac3d5fead04b59981fbce801007e"
 
-  url "https://github.com/ouladck/LumiNest/releases/download/v#{version}/LumiNest-#{version}.zip"
+  url "https://github.com/ouladck/LumiNest/releases/download/#{version}/LumiNest-#{version}.zip"
   name "LumiNest"
   desc "Fast local photo and video browser for macOS"
   homepage "https://github.com/ouladck/LumiNest"
